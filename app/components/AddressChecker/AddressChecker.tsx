e4c0d1768dc1c8cac48e955d5e3b272c26f82118
andytudhope <13001517+andytudhope@users.noreply.github.com>
2024-06-03 16:50:13 +0200

    Fixes input field and adjusts styles

--- app/components/AddressChecker/AddressChecker.tsx
+++ app/components/AddressChecker/AddressChecker.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
 import { useDataContext } from '../../context/DataContext';
 import { useFetchCSV } from '../../lib/fetchCSV';
+import { Alchemy, Network } from 'alchemy-sdk';
+
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
 
 const AddressChecker: React.FC = () => {
+  const [input, setInput] = useState('');
   const [address, setAddress] = useState('');
   const [checked, setChecked] = useState(false);
   const [totalRefund, setTotalRefund] = useState<number | null>(null);
@@ -30,8 +38,35 @@ const AddressChecker: React.FC = () => {
     return `${year}-${month}-${day}`;
   };
 
+  const resolveENS = async (name: string) => {
+    try {
+      const resolvedAddress = await alchemy.core.resolveName(name);
+      if (resolvedAddress) {
+        return resolvedAddress;
+      }
+    } catch (error) {
+      console.error('Error resolving ENS name:', error);
+    }
+    return null;
+  };
+
   const handleCheckAddress = async () => {
-    const dates = getDateRange(90); // we fetch 90 days of data for some kind of completeness, while keeping it light enough to run
+    let resolvedAddress = input;
+
+    if (input.endsWith('.eth')) {
+      const ensAddress = await resolveENS(input);
+      if (ensAddress) {
+        resolvedAddress = ensAddress;
+      } else {
+        alert('ENS name could not be resolved.');
+        return;
+      }
+    }
+
+    setAddress(resolvedAddress);
+    console.log(resolvedAddress);
+
+    const dates = getDateRange(90); // Fetch 90 days of data for completeness
     const missingDates = dates.filter(date => !state.data.some(transaction => transaction.block_time.startsWith(date)));
 
     if (missingDates.length > 0) {
@@ -40,7 +75,7 @@ const AddressChecker: React.FC = () => {
       setLoading(false);
     }
 
-    const matchingTransactions = state.data.filter(transaction => transaction.user_tx_from === address);
+    const matchingTransactions = state.data.filter(transaction => transaction.user_tx_from === resolvedAddress);
     const totalRefundValue = matchingTransactions.reduce((total, transaction) => {
       return total + parseFloat(transaction.refund_value_eth);
     }, 0);
@@ -50,13 +85,13 @@ const AddressChecker: React.FC = () => {
   };
 
   return (
-    <div className="absolute top-[120px] md:top-[350px] w-4/5 md:w-full left-1/2 transform -translate-x-1/2 flex flex-col items-center bg-black border-2 border-white rounded-lg p-5" style={{ zIndex: '1', maxWidth: '800px', height: '250px' }}>
+    <div className="absolute top-[120px] md:top-[350px] w-4/5 md:w-full h-[330px] md:h-[250px] left-1/2 transform -translate-x-1/2 flex flex-col items-center bg-black border-2 border-white rounded-lg p-5" style={{ zIndex: '1', maxWidth: '800px' }}>
       <div className="flex flex-col md:flex-row mb-2.5 w-full">
         <input
           type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder="Enter your address"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Address or ENS name"
           className="border border-gray-300 p-2 rounded mb-2.5 md:mb-0 mr-0 md:mr-2.5 flex-grow"
           style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
         />
@@ -68,26 +103,31 @@ const AddressChecker: React.FC = () => {
           </button>
         )}
       </div>
-      <div className="mt-2 md:mt-8 text-white text-2xl" style={{ visibility: checked ? 'hidden' : 'visible' }}>
-        <p>See what you've saved with Protect</p>
-      </div>
-      <div className="my-2 text-white" style={{ visibility: checked ? 'visible' : 'hidden' }}>
-        <p>Total Refund: {totalRefund} ETH</p>
-      </div>
-      <div className="my-2 text-white" style={{ visibility: checked && totalRefund === 0 ? 'visible' : 'hidden' }}>
-        <p>No transactions found for this address in the last 3 months.</p>
-      </div>
-      <div className="mt-6" style={{ visibility: checked ? 'visible' : 'hidden' }}>
-        <a
-          href="https://protect.flashbots.net"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="p-4 rounded text-white border-none cursor-pointer no-underline hover:bg-opacity-75 disabled:bg-gray-300 disabled:cursor-not-allowed"
-          style={{ backgroundColor: 'color(display-p3 0.37 0.1073 0.6327)' }}
-        >
-          Get Protected!
-        </a>
-      </div>
+      {!checked ? (
+        <div className="mt-2 md:mt-8 text-white text-2xl">
+          <p>See what you've saved with Protect</p>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+          <div className="my-2 text-white">
+            <p>Total Refund: {totalRefund} ETH</p>
+          </div>
+          <div className="my-2 text-white" style={{ visibility: totalRefund === 0 ? 'visible' : 'hidden' }}>
+            <p>No transactions found for this address in the last 3 months.</p>
+          </div>
+          <div className="mt-6">
+            <a
+              href="https://protect.flashbots.net"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-4 rounded text-white border-none cursor-pointer no-underline hover:bg-opacity-75 disabled:bg-gray-300 disabled:cursor-not-allowed"
+              style={{ backgroundColor: 'color(display-p3 0.37 0.1073 0.6327)' }}
+            >
+              Get Protected!
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- app/components/DatePicker.tsx
+++ app/components/DatePicker.tsx
@@ -15,21 +15,21 @@ const DatePicker: React.FC<DatePickerProps> = ({ onDateChange }) => {
   return (
     <div className='text-center'>
       <button
-        className={`border rounded my-4 mr-4 py-2 px-4 ${activeRange === 'today' ? 'bg-white text-black' : 'text-white'}`}
+        className={`border rounded my-4 mr-2 md:mr-4 py-2 px-4 text-[10px] md:text-sm ${activeRange === 'today' ? 'bg-white text-black' : 'text-white'}`}
         style={activeRange === 'today' ? { borderColor: 'color(display-p3 0.37 0.1073 0.6327)' } : {}}
         onClick={() => handleDateChange('today')}
       >
         Today
       </button>
       <button
-        className={`border rounded my-4 mr-4 py-2 px-4 ${activeRange === 'this_week' ? 'bg-white text-black' : 'text-white'}`}
+        className={`border rounded my-4 mr-2 md:mr-4 py-2 px-4 text-[10px] md:text-sm  ${activeRange === 'this_week' ? 'bg-white text-black' : 'text-white'}`}
         style={activeRange === 'this_week' ? { borderColor: 'color(display-p3 0.37 0.1073 0.6327)' } : {}}
         onClick={() => handleDateChange('this_week')}
       >
         This Week
       </button>
       <button
-        className={`border rounded my-4 mr-4 py-2 px-4 ${activeRange === 'this_month' ? 'bg-white text-black' : 'text-white'}`}
+        className={`border rounded my-4 mr-2 md:mr-4 py-2 px-4 text-[10px] md:text-sm  ${activeRange === 'this_month' ? 'bg-white text-black' : 'text-white'}`}
         style={activeRange === 'this_month' ? { borderColor: 'color(display-p3 0.37 0.1073 0.6327)' } : {}}
         onClick={() => handleDateChange('this_month')}
       >
